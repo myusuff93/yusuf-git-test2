8966b7b02fe92429fba4dd725c139c8c6facff2e
myusuff93 <muhamadyusupfajarudin@gmail.com>
2022-10-28 06:27:41 +0700

    menambahkan tag section, article, dan p

--- index.php
+++ index.php
@@ -8,5 +8,10 @@
 </head>
 <body>
     <h3>Hello World</h3>
+    <section>
+        <article>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque minima atque ipsa explicabo reprehenderit, commodi nihil hic maxime aut dicta unde accusantium quae sint quia natus quibusdam at ut sequi quisquam consequuntur dolores nam? Magni quis tenetur officiis natus rem officia excepturi aspernatur, vel velit assumenda iste cumque temporibus illo, neque et. Repellendus doloribus exercitationem iusto alias sint enim quam porro iure blanditiis cum repudiandae id fuga placeat delectus dolores qui praesentium culpa, voluptate deleniti. Excepturi dicta incidunt, nihil facere ut inventore minus odit assumenda atque quas soluta numquam dolore vero quisquam omnis cumque exercitationem beatae cupiditate ex! Mollitia, alias.</p>
+        </article>
+    </section>
 </body>
 </html>
